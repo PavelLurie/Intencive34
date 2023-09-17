789b38795281823693ad2d15b24a8fcd3ccdf6a2
PavelLurie <paul.lurie@yandex.ru>
2023-09-17 10:21:41 +0300

    Создать три стрима из массивов чисел от 1 до 10, от 10 до 20 и от 20 до 30 соответственно. Объединить их в один стрим и вывести числа, которые кратны 5.

---
+++ src/test/java/task6/Objective41.java
@@ -0,0 +1,26 @@
+package task6;
+/*
+Задача №41
+Создать три стрима из массивов чисел от 1 до 10, от 10 до 20 и от 20 до 30 соответственно.
+Объединить их в один стрим и вывести числа, которые кратны 5.
+ */
+
+import java.util.Arrays;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+public class Objective41 {
+    public static void main(String[] args) {
+        Stream<Integer> stream = Stream.iterate(1, s -> s + 1)
+                .limit(10);
+        Stream<Integer> stream1 = Stream.iterate(11, s -> s + 1)
+                .limit(10);
+        Stream<Integer> stream2 = Stream.iterate(21, s -> s +1)
+                .limit(10);
+
+        Stream.of(stream, stream1, stream2)
+                .flatMap(Function.identity())
+                .filter(s -> s % 5 == 0)
+                .forEach(System.out::println);
+    }
+}
